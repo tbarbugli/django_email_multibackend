537883d6fd741a5cde72472368cbc03ac393dac3
Tommaso Barbugli <tbarbugli@gmail.com>
2015-03-04 11:25:47 -0500

    release 0.5.0

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Framework :: Django',
     'Environment :: Web Environment',
